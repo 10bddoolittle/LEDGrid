6daedd7832db437908f616ea7b7b35f03d2279fe
Brian Doolittle <10bddoolittle@gmail.com>
2015-11-13 22:47:31 -0500

    color Led

--- Color_Display_Script.py
+++ Color_Display_Script.py
@@ -1,5 +1,3 @@
-__author__ = 'briandoolittle'
-
 import Display_Module
 
 rowgpios = ["P8_7","P8_9","P8_11"]
@@ -8,10 +6,32 @@ colgpios = ["P8_8"]
 display = Display_Module.Display(rowgpios,colgpios)
 
 
-array1 = [[0],
-		  [1],
-		  [0]]
+red = [[0],[0],[1]]
+blue = [[0],[1],[0]]
+green = [[1],[0],[0]]
+purple = [[0],[1],[1]]
+yellow = [[1],[0],[1]]
+cyan = [[1],[1],[0]]
+white = [[1],[1],[1]]
+black = [[0],[0],[0]]
+
+runtime = .6
+flickertime = .002
+flashtime = .15
 
 while True:
-    display.run(array1, 2, 2)
+    display.run(red, runtime, flickertime)
+    display.run(white, flashtime, flickertime)
+    display.run(purple, runtime, flickertime)
+    display.run(white, flashtime, flickertime)
+    display.run(blue, runtime, flickertime)
+    display.run(white, flashtime, flickertime)
+    display.run(cyan, runtime, flickertime)
+    display.run(white, flashtime, flickertime)
+    display.run(green, runtime, flickertime)
+    display.run(white, flashtime, flickertime)
+    display.run(yellow, runtime, flickertime)
+    display.run(white, flashtime, flickertime)
+    
+
     print "meow"
