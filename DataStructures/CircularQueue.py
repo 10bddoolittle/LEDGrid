43c9e3f8036c1037749c71928ad8a1b9b67c2e67
Brian Doolittle <10bddoolittle@gmail.com>
2015-11-18 23:33:44 -0500

    added a circular array class, untested

--- DataStructures/CircularQueue.py
+++ DataStructures/CircularQueue.py
@@ -17,3 +17,63 @@ class CircularQueue:
     # GetHead: returns the head of self.queue
     def getHead(self):
         return self.queue[0]
+
+class CircularArray:
+
+    def __init__(self,array):
+        self.array = array
+        self.numrows = len(self.array)
+        self.numcols = len(self.array[0])
+
+    def shiftLeft(self,rowids=[]):
+        if rowids == []:
+            rowids = range(0,self.numrows)
+
+        for row in rowids:
+            self.array[row] = self.array[row][1:self.numcols] + [self.array[row][0]]
+
+    def shiftRight(self,rowids=[]):
+        if rowids == []:
+            rowids = range(0,self.numrows)
+        
+        for row in rowids:
+            self.array[row] = [self.array[row][self.numcols]] + self.array[row][0:self.numcols-1]
+
+    def shiftUp(self,colids=[]):
+        if colids = []:
+            colids = range(0,self.numcols)
+
+        temparray = self.array
+
+        for col in colids:
+            for row in range(0,self.numrows):
+
+                newrow = row - 1
+
+                if newrow < 0:
+                    newrow = self.numrows-1
+
+                self.array[row][col] = temparray[newrow][col]
+
+    def shiftDown(self,colids=[]):
+        if colids = []:
+                colids = range(0,self.numcols)
+
+        temparray = self.array
+
+        for col in colids: 
+            for row in range(0,self.numrows):
+
+                newrow = row + 1
+
+                if newrow == self.numrows:
+                    newrow = 0
+                    
+                self.array[row][col] = temparray[newrow][col]
+
+
+
+
+
+
+
